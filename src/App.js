b6fcf28595f21e2cea4f15024871c2228ca3da5b
root <admin@example.com>
2022-07-09 20:36:48 +0300

    fix:(portfolio)

--- src/App.js
+++ src/App.js
@@ -67,13 +67,13 @@ function App() {
             color="primary"
             onClick={loginForm}>
             Log in
-        </Button>
+          </Button>
           <Button
             variant="outlined"
             color="primary"
             onClick={logoutForm}>
             Log out
-        </Button>
+          </Button>
           <p>or</p>
           <Link to="/reguser">Registration</Link>
         </div>
